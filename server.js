716d8a28b47737387aa3ca47b19408bf9911d7d7
Caio <cgmalavazidariva@gmail.com>
2021-09-14 20:59:21 -0300

    add cors

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDb = require("./src/database/database");
-const routes = require("./src/routes/routes")
+const routes = require("./src/routes/routes");
+var cors = require('cors');
 
 connectToDb();
 
-const app = express();
 const port = 3000;
-
+const app = express();
 app.use(express.json());
+
+app.use(cors()); 
+app.options("*", cors());
+
 app.use(routes);
 
 app.listen(port, () => console.info(`Servidor rodando em http://localhost:${port}`));
